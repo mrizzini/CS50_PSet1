9732b9a966bb20162fbbe4563ab7a3f9923dbfa3
Matthew Rizzini <matthewrizzini@gmail.com>
2018-03-29 15:50:55 +0000

    completed PSet 1

--- mario/less/mario.c
+++ mario/less/mario.c
@@ -4,42 +4,34 @@
 int main(void)
 {
     int n; // declaring variable n for use later
-    int space = 2;
-  //  char brick;
-  //  int space;
+    int brick = 2;
+    char *s = " ";
+    int count;
 
-    // Prompt user for a number between 0 and 23
+
+    // Prompt user for a number between 0 and 23. Assigns count to the user input for height - 1, to create enough spaces
     do
     {
         n = get_int("Height: ");
-     //   space = n;
+        count = n -1;
     }
-    while (n <= 0 || n >= 23);
+    while (n < 0 || n > 23);
 
     // Print this many rows
     for (int i = 0; i < n; i++)
     {
+        // Prints out the correct amount of spaces for the first row of bricks, using the count variable
+        printf("%*.s", count, s);
         // Print out this many columns
-        for (int j = 0; j < space; j++)
+        for (int j = 0; j < brick; j++)
         {
             printf("#");
+
         }
+        // Prints a line break after the row is complete, then increments brick and decrements count, to ensure next row is correct
         printf("\n");
-        space++;
+        brick++;
+        count--;
     }
 
 }
-      //  space = n - 1;
-
-     //   brick = "#";
-   //   char brick[] = "##";
-     //   printf("%s\n", brick);
-    //    n = space;
-    //    printf("%is%s\n", space, brick);
-    //    printf("%i\n", space);
-
-    //    n = space;
-
-   //     brick += "#";
-
-       // printf("%i\n", space);
